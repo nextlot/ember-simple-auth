76bce8d788e02c59dfad3f5be233bb8b8e3d3c3e
Lawrence Davis <lawrence@kohactive.com>
2015-08-06 10:45:23 -0500

    Register localStorage session-store

--- addon/initializers/setup-session.js
+++ addon/initializers/setup-session.js
@@ -1,7 +1,9 @@
 import Session from '../session';
+import LocalStorage from '../stores/local-storage';
 import Configuration from '../configuration';
 
 export default function setupSession(registry) {
+  registry.register('simple-auth-session-store:local-storage', LocalStorage);
   registry.register('simple-auth-session:main', Session);
   registry.injection('simple-auth-session:main', 'store', Configuration.base.store);
 }
