f47302a38f1abaeea0c4c07e5b42f515987b84de
Lawrence Davis <lawrence@kohactive.com>
2015-08-07 11:13:43 -0500

    Use ajaxPreFilter for authorization and allow custom sessionService

--- app/instance-initializers/simple-auth.js
+++ app/instance-initializers/simple-auth.js
@@ -1,8 +1,12 @@
 import setupSessionRestoration from 'ember-simple-auth/instance-initializers/setup-session-restoration';
+import setupAjaxPrefilter from 'ember-simple-auth/instance-initializers/setup-ajax-prefilter';
+import ENV from '../config/environment';
 
 export default {
   name:       'simple-auth',
   initialize: function(instance) {
-    setupSessionRestoration(instance);
+    var config = ENV['simple-auth'];
+    setupSessionRestoration(instance, config);
+    setupAjaxPrefilter(instance);
   }
 };
