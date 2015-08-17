6a09aaec48a107f819831c7af8d82bcbcf1bee83
Lawrence Davis <lawrence@kohactive.com>
2015-08-17 15:04:14 -0500

    Remove console.log()

--- addon/initializers/setup-session-service.js
+++ addon/initializers/setup-session-service.js
@@ -1,5 +1,3 @@
 export default function setupSessionStore(registry, config) {
-  console.log(config.base.sessionService);
-  console.log(config.base.session);
   registry.injection(config.base.sessionService, 'session', config.base.session);
 }
